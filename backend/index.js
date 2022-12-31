c765bc168a9db3780834d0bc6eed76379c29c54c
Harjot Singh <hsbinsardari12g@outlook.com>
2022-12-30 23:07:51 -0500

    db error solved

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,6 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(() => {
     console.log("successfully connected to DB");

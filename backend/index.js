a0098c27f3580439a36c21f81d56108fc1441b02
Harjot Singh <hsbinsardari12g@outlook.com>
2022-12-30 21:10:31 -0500

    path added

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("successfully connected to DB");
+    console.log(path.join(__dirname, "../frontend/build/index.html"));
   })
   .catch((err) => console.log(err));
 

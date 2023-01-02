5413ca3da8f421c91ca50fd6e88790b4b54ce671
Harjot Singh <hsbinsardari12g@outlook.com>
2023-01-02 00:45:37 -0500

    cookie preferences changed

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ecom-project-demo.vercel.app/"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"

a1ee01b621060af60584ce5ea87e8e954b6e6ced
Harjot Singh <hsbinsardari12g@outlook.com>
2023-12-24 20:35:41 -0500

    etag fixed

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
+app.disable("etag");
 app.use("/api", orderRouter);
 app.use("/api", customerRouter);
 app.use("/api", productRouter);

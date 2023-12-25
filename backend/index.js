71d3482e559b8963cd8934dd94294b519327a15a
Harjot Singh <hsbinsardari12g@outlook.com>
2023-12-24 20:01:53 -0500

    cors added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,19 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
